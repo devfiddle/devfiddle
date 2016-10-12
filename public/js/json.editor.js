78d461f6350c0f0eef877ac96ec545ad585ec5a6
cj-mac-mini <git@segevs.com>
2016-10-12 13:44:10 -0400

    Basic json editor

---
+++ public/js/json.editor.js
@@ -0,0 +1,78 @@
+var editor = null ;
+var ace_editor = null; 
+
+$(function(){
+
+    var container = document.getElementById("jsoneditor");
+    var options = {};
+    
+    editor = new JSONEditor(container, options);
+    
+    
+    
+    $(document)
+        .on('change', '#json_editor, #txtInput', function() {
+            applyJson();
+        })
+        .on('keyup', '#json_editor, #txtInput', function() {
+            applyJson();
+        })
+        .on('blur', '#json_editor, #txtInput', function() {
+            applyJson();
+        })
+        .on('click' , '[data-action="beautify"]', function(){
+            beautify();
+            return false;
+        })
+        .on('click' , '[data-action="uglify"]', function(){
+            uglify();
+            return false;
+        })
+    ;
+
+    ace_editor = ace.edit("json_editor");
+    ace_editor.getSession().setMode("ace/mode/json");
+
+    applyJson();
+    
+    // var json = editor.get();
+
+
+
+});
+
+
+var getContent = function() {
+    return (ace_editor != null) ? ace_editor.getValue():$('#txtInput').val() ;
+}
+
+
+var setContent = function(content) {
+    return (ace_editor != null) ? ace_editor.setValue(content):$('#txtInput').val(content);
+}
+
+var applyJson = function() {
+    var json = null ;
+    
+    var content = getContent();
+    
+    
+    try {
+        json = JSON.parse(content);
+        editor.set(json);
+        $('#jsoneditor .json-editor-error').hide();
+    }catch (err){
+        $('#jsoneditor .json-editor-error').show();
+        editor.set('##');
+    }
+    
+}
+
+
+var beautify = function() {
+    setContent(JSON.stringify(JSON.parse(getContent()), null, 2))
+}
+
+var uglify = function() {
+    setContent(JSON.stringify(JSON.parse(getContent())));
+}
